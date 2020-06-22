0d0444608d86698da1bc84eef5412c2fefaa46a8
Karen Cubero <kaaerreeene@gmail.com>
2020-06-22 15:58:26 +0200

    [GH-5] feature(storybook): add storybook without style support

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const tailwindcss = require("tailwindcss");
+
+module.exports = ({ config }) => {
+  const aliases = config.resolve.alias || (config.resolve.alias = {});
+  aliases.react = aliases["react-dom"] = "preact/compat";
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("awesome-typescript-loader"),
+      },
+      // Optional
+      {
+        loader: require.resolve("react-docgen-typescript-loader"),
+      },
+    ],
+  });
+
+  config.resolve.extensions.push(".ts", ".tsx");
+  return config;
+};
