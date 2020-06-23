1f351f927b47a625d196f391eb5c113e79e72b5e
Karen Cubero <kaaerreeene@gmail.com>
2020-06-23 18:47:10 +0200

    [GH-5] feature(storybook): add storybook

---
+++ .storybook/postcss.config.js
@@ -0,0 +1,22 @@
+// @see https://nextjs.org/docs/advanced-features/customizing-postcss-config
+// @see https://tailwindcss.com/docs/controlling-file-size/
+// Next.js has a different way of handling postcss configuration file
+module.exports = {
+  plugins: [
+    "tailwindcss",
+    "postcss-preset-env",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
+};
