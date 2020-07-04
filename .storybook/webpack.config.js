7115369f60ac202c2dd01b12a13e8b6038184d60
Tomeu Cabot <somethingofhappy@gmail.com>
2020-07-04 15:55:33 +0200

    [GH-13] fix(storybook): add custom postcss  config path

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,21 +1,26 @@
 const path = require("path");
-const tailwindcss = require("tailwindcss");
 
 module.exports = ({ config }) => {
   const aliases = config.resolve.alias || (config.resolve.alias = {});
   aliases.react = aliases["react-dom"] = "preact/compat";
 
+  config.module.rules.forEach(rule => {
+    if (rule.test.toString() === '/\\.css$/') {
+    // @see: https://github.com/storybookjs/storybook/blob/next/lib/core/src/server/preview/base-webpack.config.js#L62
+     rule.use[2].options = {
+        config: {
+          path: path.resolve(__dirname, './postcss.config.js'),
+        }
+     }
+    }
+  })
+
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("awesome-typescript-loader"),
-      },
-      // Optional
-      {
-        loader: require.resolve("react-docgen-typescript-loader"),
-      },
-    ],
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [require.resolve('babel-preset-react-app')],
+    },
   });
 
   config.resolve.extensions.push(".ts", ".tsx");
